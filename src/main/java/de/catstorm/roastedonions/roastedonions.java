84b0ed591c41455f091cdcfaf18cc054a8e8d42e
Cat <contact@catstorn.de>
2023-06-27 14:45:11 +0200

    Cleaned up my code

--- src/main/java/de/catstorm/roastedonions/roastedonions.java
+++ src/main/java/de/catstorm/roastedonions/roastedonions.java
@@ -21,64 +21,67 @@ public class roastedonions
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, "roastedonions");
 
-    public static final RegistryObject<Item> roastedonion = ITEMS.register("roastedonion",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(5).saturationMod(3.62f/20).build())));
+    public static void initItems()
+    {
+        final RegistryObject<Item> roastedonion = ITEMS.register("roastedonion",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(5).saturationMod(3.62f/20).build())));
 
-    public static final RegistryObject<Item> blue_spandauer = ITEMS.register("blue_spandauer",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(7).saturationMod(10f/20).build())));
+        final RegistryObject<Item> blue_spandauer = ITEMS.register("blue_spandauer",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(7).saturationMod(10f/20).build())));
 
-    public static final RegistryObject<Item> red_spandauer = ITEMS.register("red_spandauer",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(7).saturationMod(10f/20).build())));
+        final RegistryObject<Item> red_spandauer = ITEMS.register("red_spandauer",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(7).saturationMod(10f/20).build())));
 
-    public static final RegistryObject<Item> golden_berries = ITEMS.register("golden_berries",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(11).saturationMod(10f/20).build())));
+        final RegistryObject<Item> golden_berries = ITEMS.register("golden_berries",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(11).saturationMod(10f/20).build())));
 
-    public static final RegistryObject<Item> raw_sausage = ITEMS.register("raw_sausage",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(3).saturationMod(3.62f/20).build())));
+        final RegistryObject<Item> raw_sausage = ITEMS.register("raw_sausage",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(3).saturationMod(3.62f/20).build())));
 
-    public static final RegistryObject<Item> cooked_sausage = ITEMS.register("cooked_sausage",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(5).saturationMod(7.35f/20).build())));
+        final RegistryObject<Item> cooked_sausage = ITEMS.register("cooked_sausage",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(5).saturationMod(7.35f/20).build())));
 
-    public static final RegistryObject<Item> hotdog = ITEMS.register("hotdog",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(7).saturationMod(9f/20).build())));
+        final RegistryObject<Item> hotdog = ITEMS.register("hotdog",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(7).saturationMod(9f/20).build())));
 
-    public static final RegistryObject<Item> hotdog_with_ketchup = ITEMS.register("hotdog_with_ketchup",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(8).saturationMod(10f/20).build())));
+        final RegistryObject<Item> hotdog_with_ketchup = ITEMS.register("hotdog_with_ketchup",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(8).saturationMod(10f/20).build())));
 
-    public static final RegistryObject<Item> pig_fat = ITEMS.register("pig_fat",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-                .food(new FoodProperties.Builder().nutrition(3).saturationMod(3.000001f/20).effect(new MobEffectInstance(MobEffects.CONFUSION, 1000, 4), 0.95f).build())));
+        final RegistryObject<Item> pig_fat = ITEMS.register("pig_fat",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(3).saturationMod(3.000001f/20).effect(new MobEffectInstance(MobEffects.CONFUSION, 1000, 4), 0.95f).build())));
 
-    public static final RegistryObject<Item> pork_rind = ITEMS.register("pork_rind",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(5).saturationMod(4.694201f/20).effect(new MobEffectInstance(MobEffects.CONFUSION, 1000, 4), 0.69f).build())));
+        final RegistryObject<Item> pork_rind = ITEMS.register("pork_rind",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(5).saturationMod(4.694201f/20).effect(new MobEffectInstance(MobEffects.CONFUSION, 1000, 4), 0.69f).build())));
 
-    public static final RegistryObject<Item> uncooked_klejner = ITEMS.register("uncooked_klejner",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+        final RegistryObject<Item> uncooked_klejner = ITEMS.register("uncooked_klejner",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
-    public static final RegistryObject<Item> klejner = ITEMS.register("klejner",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(3).saturationMod(4.111111f/20).build())));
+        final RegistryObject<Item> klejner = ITEMS.register("klejner",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(3).saturationMod(4.111111f/20).build())));
 
-    public static final RegistryObject<Item> salmon_fillet = ITEMS.register("salmon_fillet",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(1).saturationMod(1f/20).build())));
+        final RegistryObject<Item> salmon_fillet = ITEMS.register("salmon_fillet",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(1).saturationMod(1f/20).build())));
 
-    public static final RegistryObject<Item> smoked_salmon = ITEMS.register("smoked_salmon",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(4).saturationMod(4f/20).build())));
+        final RegistryObject<Item> smoked_salmon = ITEMS.register("smoked_salmon",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(4).saturationMod(4f/20).build())));
 
-    public static final RegistryObject<Item> smorrebrod = ITEMS.register("smorrebrod",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(9).saturationMod(10f/20).build())));
+        final RegistryObject<Item> smorrebrod = ITEMS.register("smorrebrod",
+                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                        .food(new FoodProperties.Builder().nutrition(9).saturationMod(10f/20).build())));
+    }
 
     public roastedonions()
     {
@@ -88,6 +91,8 @@ public class roastedonions
         ITEMS.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        initItems();
     }
 
     private void setup(final FMLCommonSetupEvent event)
